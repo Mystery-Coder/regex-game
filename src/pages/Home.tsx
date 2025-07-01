8a0d72f4b3c20d9a16337ad2df13f0fa681d0982
MysteryCoder <srikar0811@gmail.com>
2025-07-01 20:02:33 +0530

    linear grad

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,13 @@ export default function Home() {
 	const navigate = useNavigate();
 	return (
 		<>
-			<Navbar className="bg-body-secondary border border-secondary border-2">
+			<Navbar
+				className="border border-secondary border-2"
+				style={{
+					background:
+						"linear-gradient(to left,rgb(236, 30, 30),rgb(246, 187, 25))",
+				}}
+			>
 				<Container fluid>
 					<Navbar.Brand className="fs-3">
 						<img
@@ -39,7 +45,7 @@ export default function Home() {
 				style={{
 					height: "calc(100vh - 56px)",
 					background:
-						"linear-gradient(to right,rgb(79, 254, 169),rgb(25, 47, 246))",
+						"linear-gradient(to right,rgb(79, 254, 169),rgb(25, 47, 246),rgb(87, 36, 255))",
 				}} // Adjust height based on navbar
 			>
 				<Button variant="info" className="m-4 fs-4">
