25aea9694e032c662066a8950cd47660c4980510
MysteryCoder <srikar0811@gmail.com>
2025-09-15 18:11:23 +0530

    about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,23 @@
+import { Box, Typography } from "@mui/material";
+
 export default function About() {
-	return <div>About</div>;
+	return (
+		<Box className="center-content">
+			<Typography variant="h2">Regex Battle</Typography>
+			<Typography variant="h5">
+				Hone your Regular Expression skills by battling 1v1, Practice
+				RegEx at{" "}
+				<a
+					href="https://regexr.com/"
+					target="_blank"
+					style={{
+						textDecoration: "none",
+						color: "rgba(114, 152, 255, 1)",
+					}}
+				>
+					RegExr
+				</a>
+			</Typography>
+		</Box>
+	);
 }
