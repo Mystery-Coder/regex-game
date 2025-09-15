f373633899b0036e1316955d32a2669feffeacfc
MysteryCoder <srikar0811@gmail.com>
2025-09-15 18:04:55 +0530

    ws connect

--- src/pages/GuessRegex.tsx
+++ src/pages/GuessRegex.tsx
@@ -3,6 +3,39 @@ import { useState } from "react";
 
 export default function GuessRegex() {
 	const [joinRoom, setJoinRoom] = useState(false);
+
+	const createRoomAndJoin = async () => {
+		try {
+			const res = await fetch("http://localhost:8080/create_room");
+			if (!res.ok) {
+				throw new Error("Failed to create room");
+			}
+
+			const data = await res.json();
+			console.log(data.RoomID);
+
+			const add_player_res = await fetch(
+				"http://localhost:8080/add_player",
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ RoomID: data.RoomID }),
+				}
+			);
+
+			if (!add_player_res.ok) {
+				const errorData = await add_player_res.json();
+				throw new Error(errorData.error || "Failed to add player");
+			}
+			const player_data = await add_player_res.json();
+			console.log(player_data);
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
 	return (
 		<Box className="center-content-horizontal">
 			<Typography variant="h2">Guess the Regex</Typography>
@@ -18,7 +51,11 @@ export default function GuessRegex() {
 				}}
 			>
 				{joinRoom && <TextField label="Room ID"></TextField>}
-				{!joinRoom && <Button>Create 1v1 Room</Button>}
+				{!joinRoom && (
+					<Button onClick={() => createRoomAndJoin()}>
+						Create 1v1 Room
+					</Button>
+				)}
 				<Button onClick={() => setJoinRoom(true)}>Join 1v1 Room</Button>
 			</Box>
 		</Box>
