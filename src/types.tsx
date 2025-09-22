bf236ba63151310259f98c2baf8b6255ef1292fa
MysteryCoder <srikar0811@gmail.com>
2025-09-22 10:19:41 +0530

    event types

--- src/types.tsx
+++ src/types.tsx
@@ -10,3 +10,12 @@ export interface Player {
 	PlayerID: string;
 	RoomID: string;
 }
+
+export interface RoomStatus {
+	Status: string;
+}
+
+export interface Message {
+	Type: string;
+	Data: RoomStatus;
+}
